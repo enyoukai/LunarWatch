0ef962d00ba56fd6af16b7d132bd74e761d7d4f8
Enyoukai <52297896+enyoukai@users.noreply.github.com>
2023-06-28 21:02:17 -0700

    clock in lvgl

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,9 +44,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-void LCDInit();
-void LCDBootScreen();
-void LCDRenderClock();
+void GC9A01AInit();
+void LVGLInit();
+void BootScreen();
+void renderClock(lv_timer_t * timer);
 void my_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
 /* USER CODE END PD */
 
@@ -65,7 +66,16 @@ void my_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-char messageBuffer[20];
+static lv_obj_t * secondsLine;
+static lv_obj_t * minutesLine;
+static lv_obj_t * hoursLine;
+
+static lv_point_t secondsLinePoints[] = { {120, 120}, {120, 120 - SECONDS_RADIUS} };
+static lv_point_t minutesLinePoints[] = { {120, 120}, {120, 120 - MINUTES_RADIUS } };
+static lv_point_t hoursLinePoints[] = { {120, 120}, {120, 120 - HOURS_RADIUS} };
+
+static lv_obj_t * timeLabel;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -133,35 +143,83 @@ int main(void)
   MX_RTC_Init();
   /* USER CODE BEGIN 2 */
 
-  LCDInit();
-  LCDBootScreen();
-
-  Paint_Clear(WHITE);
-
-  lv_init();
-  lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, LVGL_BUFF_SIZE);
-
-  static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
-  lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
-  disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
-  disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
-  disp_drv.hor_res = LCD_1IN28_WIDTH;                 /*Set the horizontal resolution in pixels*/
-  disp_drv.ver_res = LCD_1IN28_HEIGHT;                 /*Set the vertical resolution in pixels*/
-
-  lv_disp_t * disp;
-  disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+  GC9A01AInit();
+  LVGLInit();
 
   // Change the active screen's background color
   lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57), LV_PART_MAIN);
+  lv_obj_clean(lv_scr_act());
+
+  lv_obj_t * label = lv_label_create(lv_scr_act());
+  lv_label_set_text(label, "LunarClock");
   lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
+  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-  /*Create a spinner*/
-  lv_obj_t * spinner = lv_spinner_create(lv_scr_act(), 1000, 60);
-  lv_obj_set_size(spinner, 64, 64);
-  lv_obj_align(spinner, LV_ALIGN_BOTTOM_MID, 0, 0);
+  static lv_style_t st;
+  lv_style_init(&st);
+  lv_style_set_text_font(&st, &lv_font_montserrat_24);
 
-//	Paint_DrawImage(gImage_70X70, 85, 25, 70, 70);
-//	Paint_DrawString_CN(56,140, "΢ѩ����",   &Font24CN,BLACK,  WHITE);
+  lv_obj_add_style(label, &st, LV_PART_MAIN);
+
+  for( int i=0; i < 600; i++ ){
+        lv_task_handler();
+        HAL_Delay(5);
+    }
+
+  lv_obj_del(label);
+
+  static lv_style_t hoursLineStyle;
+  /*Create style*/
+  lv_style_init(&hoursLineStyle);
+  lv_style_set_line_width(&hoursLineStyle, 8);
+  lv_style_set_line_color(&hoursLineStyle, lv_color_white());
+  lv_style_set_line_rounded(&hoursLineStyle, true);
+
+  /*Create a line and apply the new style*/
+  hoursLine = lv_line_create(lv_scr_act());
+  lv_line_set_points(hoursLine, hoursLinePoints, 2);     /*Set the points*/
+  lv_obj_add_style(hoursLine, &hoursLineStyle, 0);
+
+  static lv_style_t minutesLineStyle;
+  /*Create style*/
+  lv_style_init(&minutesLineStyle);
+  lv_style_set_line_width(&minutesLineStyle, 8);
+  lv_style_set_line_color(&minutesLineStyle, lv_color_white());
+  lv_style_set_line_rounded(&minutesLineStyle, true);
+
+  /*Create a line and apply the new style*/
+  minutesLine = lv_line_create(lv_scr_act());
+  lv_line_set_points(minutesLine, minutesLinePoints, 2);     /*Set the points*/
+  lv_obj_add_style(minutesLine, &minutesLineStyle, 0);
+
+  static lv_style_t secondsLineStyle;
+  /*Create style*/
+  lv_style_init(&secondsLineStyle);
+  lv_style_set_line_width(&secondsLineStyle, 2);
+  lv_style_set_line_color(&secondsLineStyle, lv_palette_main(LV_PALETTE_BLUE));
+  lv_style_set_line_rounded(&secondsLineStyle, true);
+
+  /*Create a line and apply the new style*/
+  secondsLine = lv_line_create(lv_scr_act());
+  lv_line_set_points(secondsLine, secondsLinePoints, 2);     /*Set the points*/
+  lv_obj_add_style(secondsLine, &secondsLineStyle, 0);
+
+  timeLabel = lv_label_create(lv_scr_act());
+  lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
+  lv_obj_align(timeLabel, LV_ALIGN_CENTER, 0, 50);
+
+  static lv_style_t timeLabelStyle;
+  lv_style_init(&timeLabelStyle);
+  lv_style_set_text_font(&timeLabelStyle, &lv_font_montserrat_24);
+  lv_label_set_text(timeLabel, "");
+
+  lv_obj_add_style(timeLabel, &timeLabelStyle, LV_PART_MAIN);
+
+  lv_timer_t * renderClockTimer = lv_timer_create_basic();
+  lv_timer_set_cb(renderClockTimer, renderClock);
+  lv_timer_set_period(renderClockTimer, 1000);
+
+  lv_timer_ready(renderClockTimer);
 
 
   /* USER CODE END 2 */
@@ -173,11 +231,9 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
 	  lv_timer_handler();
 	  HAL_Delay(5);
-
-
-
   }
   /* USER CODE END 3 */
 }
@@ -233,7 +289,7 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
-void LCDInit() {
+void GC9A01AInit() {
 	DEV_Module_Init();
 
 	LCD_1IN28_SetBackLight(10000);
@@ -247,30 +303,41 @@ void LCDInit() {
 	Paint_SetDisplayFuntion(LCD_1IN28_DrawPaint);
 }
 
-void LCDBootScreen() {
+void LVGLInit()
+{
+	  lv_init();
+	  lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, LVGL_BUFF_SIZE);
+
+	  static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
+	  lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
+	  disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
+	  disp_drv.flush_cb = my_flush_cb;        /*Set a flush callback to draw to the display*/
+	  disp_drv.hor_res = LCD_1IN28_WIDTH;                 /*Set the horizontal resolution in pixels*/
+	  disp_drv.ver_res = LCD_1IN28_HEIGHT;                 /*Set the vertical resolution in pixels*/
+
+	  lv_disp_t * disp;
+	  disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+}
+
+void BootScreen() {
 	Paint_Clear(WHITE);
 	Paint_DrawString_EN(56, 120, "LunarWatch",&Font24,  WHITE, BLUE);
 
 	HAL_Delay(2000);
 }
 
-void LCDRenderClock() {
-	  Paint_Clear(WHITE);
-
+void renderClock(lv_timer_t* timer) {
 	RTC_DateTypeDef getDate = {0};
 	RTC_TimeTypeDef getTime = {0};
-	if (HAL_RTC_GetTime(&hrtc, &getTime, RTC_FORMAT_BIN) != HAL_OK)
-	{
-	Error_Handler();
-	}
-	if (HAL_RTC_GetDate(&hrtc, &getDate, RTC_FORMAT_BIN) != HAL_OK)
-	{
-	Error_Handler();
-	}
-	printf("%02d:%02d:%02d\n", getTime.Hours, getTime.Minutes, getTime.Seconds);
-	printf("%02d/%02d/%02d\n", getDate.Date, getDate.Month, getDate.Year);
 
-	sprintf(messageBuffer, "%d:%02d", getTime.Hours, getTime.Minutes);
+    if (HAL_RTC_GetTime(&hrtc, &getTime, RTC_FORMAT_BIN) != HAL_OK)
+    {
+      Error_Handler();
+    }
+    if (HAL_RTC_GetDate(&hrtc, &getDate, RTC_FORMAT_BIN) != HAL_OK)
+    {
+      Error_Handler();
+    }
 
 	float secondsDeg = (360 * (float)getTime.Seconds) / 60;
 	float secondsRad = degToRad(secondsDeg);
@@ -281,13 +348,20 @@ void LCDRenderClock() {
 	float hoursDeg = (360 * (float)getTime.Hours) / 24; // military time
 	float hoursRad = degToRad(hoursDeg);
 
-	Paint_DrawLine  (120, 120, 120 + HOURS_RADIUS * sin(hoursRad), 120 - HOURS_RADIUS * cos(hoursRad),BLACK ,DOT_PIXEL_3X3,LINE_STYLE_SOLID); // hour
-	Paint_DrawLine  (120, 120, 120 + MINUTES_RADIUS * sin(minutesRad), 120 - MINUTES_RADIUS * cos(minutesRad),BLACK ,DOT_PIXEL_3X3,LINE_STYLE_SOLID); // minute
-	Paint_DrawLine  (120, 120, 120 + SECONDS_RADIUS * sin(secondsRad), 120 - SECONDS_RADIUS * cos(secondsRad),RED ,DOT_PIXEL_2X2,LINE_STYLE_SOLID); // seconds
+	hoursLinePoints[1].x = 120 +  HOURS_RADIUS * sin(hoursRad);
+	hoursLinePoints[1].y = 120 - HOURS_RADIUS * cos(hoursRad);
 
-	Paint_DrawString_EN(56, 120, messageBuffer,&Font24,  WHITE, BLUE);
+	minutesLinePoints[1].x = 120 +  MINUTES_RADIUS * sin(minutesRad);
+	minutesLinePoints[1].y = 120 - MINUTES_RADIUS * cos(minutesRad);
 
-	HAL_Delay(1000);
+	secondsLinePoints[1].x = 120 +  SECONDS_RADIUS * sin(secondsRad);
+	secondsLinePoints[1].y = 120 - SECONDS_RADIUS * cos(secondsRad);
+
+	lv_line_set_points(hoursLine, hoursLinePoints, 2);     /*Set the points*/
+	lv_line_set_points(minutesLine, minutesLinePoints, 2);     /*Set the points*/
+	lv_line_set_points(secondsLine, secondsLinePoints, 2);     /*Set the points*/
+
+	lv_label_set_text_fmt(timeLabel, "%d : %02d", getTime.Hours, getTime.Minutes);
 }
 
 
